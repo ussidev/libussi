b2b665d7edb057b35f88ad1bd4cc7d6be0e5f3fd
Maman Tirta Rukmana <kangmaman@gmail.com>
2020-03-06 05:43:09 +0700

    revisi package config, penambahan function GetEnv

--- config/config.go
+++ config/config.go
@@ -6,53 +6,26 @@ import (
 	"strings"
 )
 
-type GitHubConfig struct {
-	Username string
-	APIKey   string
-}
-
-type Config struct {
-	GitHub    GitHubConfig
-	DebugMode bool
-	UserRoles []string
-	MaxUsers  int
-}
-
-// New returns a new Config struct
-func New() *Config {
-	return &Config{
-		GitHub: GitHubConfig{
-			Username: getEnv("GITHUB_USERNAME", ""),
-			APIKey:   getEnv("GITHUB_API_KEY", ""),
-		},
-		DebugMode: getEnvAsBool("DEBUG_MODE", true),
-		UserRoles: getEnvAsSlice("USER_ROLES", []string{"admin"}, ","),
-		MaxUsers:  getEnvAsInt("MAX_USERS", 1),
-	}
-}
-
-// Simple helper function to read an environment or return a default value
-func getEnv(key string, defaultVal string) string {
+// GetEnv : function to read an environment or return a default value
+func GetEnv(key string, defaultVal string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
-
 	return defaultVal
 }
 
-// Simple helper function to read an environment variable into integer or return a default value
-func getEnvAsInt(name string, defaultVal int) int {
-	valueStr := getEnv(name, "")
+// GetEnvAsInt : function to read an environment variable into integer or return a default value
+func GetEnvAsInt(name string, defaultVal int) int {
+	valueStr := GetEnv(name, "")
 	if value, err := strconv.Atoi(valueStr); err == nil {
 		return value
 	}
-
 	return defaultVal
 }
 
-// Helper to read an environment variable into a bool or return default value
-func getEnvAsBool(name string, defaultVal bool) bool {
-	valStr := getEnv(name, "")
+// GetEnvAsBool : function to read an environment variable into a bool or return default value
+func GetEnvAsBool(name string, defaultVal bool) bool {
+	valStr := GetEnv(name, "")
 	if val, err := strconv.ParseBool(valStr); err == nil {
 		return val
 	}
@@ -60,15 +33,24 @@ func getEnvAsBool(name string, defaultVal bool) bool {
 	return defaultVal
 }
 
-// Helper to read an environment variable into a string slice or return default value
-func getEnvAsSlice(name string, defaultVal []string, sep string) []string {
-	valStr := getEnv(name, "")
-
+// GetEnvAsSlice : function to read an environment variable into a string slice or return default value
+// Print out each role
+//for _, role := range conf.UserRoles {
+//	fmt.Println(role)
+//    }
+func GetEnvAsSlice(name string, defaultVal []string, sep string) []string {
+	valStr := GetEnv(name, "")
 	if valStr == "" {
 		return defaultVal
 	}
-
 	val := strings.Split(valStr, sep)
-
 	return val
 }
+
+/*
+GITHUB_USERNAME=craicoverflow
+GITHUB_API_KEY=TCtQrZizM1xeo1v92lsVfLOHDsF7TfT5lMvwSno
+MAX_USERS=10
+USER_ROLES_SLICE=admin,super_admin,guest
+DEBUG_MODE=false
+*/
